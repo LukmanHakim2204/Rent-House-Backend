2ce721ef29e79a9c589253afeb0e5a4fe389b210
Lukman <103345757+LukmanHakim2204@users.noreply.github.com>
2024-11-15 15:27:11 +0700

    create API logout, login, and fetch user data, listings

---
+++ app/Http/Controllers/API/ListingController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use App\Http\Controllers\Controller;
+
+class ListingController extends Controller
+{
+    public function index(): JsonResponse
+    {
+        $listings = Listing::withCount('transaction')->orderBy('transaction_count', 'desc')->paginate(); // mengambil listing yang favorites
+        return response()->json([
+            'success' => true,
+            'message' => 'Get All Listings',
+            'data' => $listings
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\ListingController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+    return response()->json([
+        'success' => true,
+        'message' => ' Detail Login User Success',
+        'data' => $request->user()
+    ]);
 });
+
+Route::resource('listing', ListingController::class)->only(['index']);
 require __DIR__ . '/auth.php';
